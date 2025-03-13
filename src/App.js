efdfad83e94a910151fb8be3e7df40b143cb6229
dennyong07 <dennyong07@gmail.com>
2025-03-13 10:59:37 +0700

    frist

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [todos, setTodos] = useState([])
+  const [input, setInput] = useState('')
+
+  const addTodo = () => {
+    if(input === '') {
+      alert('Please enter a task')
+      return
+    }
+    setTodos([...todos, input])
+    setInput('')
+  }
+
+  const checkboxClicked = (index) => {
+    alert(`Task ${todos[index]} is completed`)
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My Todo</h1>
+        <p>{new Date().toLocaleString('en-US', { weekday: 'long', month: 'long', day: 'numeric' })}</p>
+      </header>
+      <div className="Todo-list">
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index}>
+                <input 
+                  type="checkbox"
+                  onChange={() => checkboxClicked(index)}
+                />
+
+                <button className="edit-task">Edit</button>
+            {todo}</li>
+          ))}
+        </ul>
+          <div>
+            <input className="Input-task"
+              type="text"
+              value={input}
+              onChange={e => setInput(e.target.value)}
+              placeholder="Add New Task" 
+             />
+            <button className="Add-task"onClick={addTodo}>Add Task</button>
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
